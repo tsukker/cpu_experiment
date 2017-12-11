d9f107ffa83e14406ea063e36bca4e8b38b35714
Shunsuke Tsukamoto <stutaw+github@gmail.com>
2017-12-11 23:14:15 +0900

    Complete alu.v

--- cpu/cpu.srcs/sources_1/new/alu.v
+++ cpu/cpu.srcs/sources_1/new/alu.v
@@ -24,10 +24,23 @@ module alu(
     input [31:0] inLeft,
     input [31:0] inRight,
     input [5:0] op,
-    input [10:0] aux,
     output [31:0] result
     );
+    reg [31:0] res;
 
-    assign result = inLeft + inRight;
+    case (op)
+        5'd0: res = inLeft + inRight;
+        5'd2: res = inLeft - inRight;
+        5'd8: res = inLeft & inRight;
+        5'd9: res = inLeft | inRight;
+        5'd10: res = inLeft ^ inRight;
+        5'd11: res = ~(inLeft | inRight);
+        5'd16: res = inLeft << shift;
+        5'd17: res = inLeft >> shift;
+        5'd18: res = inLeft >>> shift;
+        default: res = 32'hffffffff;
+    endcase
+
+    assign result = res;
 
 endmodule
